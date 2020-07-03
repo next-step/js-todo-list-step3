710e904b586c5969a7e548bcbf6a186a033ab6c2
ganeodolu <ganeodolu@gmail.com>
2020-07-03 14:11:35 +0900

    feat: teamInfo fetch시 try catch 적용

--- js/kanbanMemberList.js
+++ js/kanbanMemberList.js
@@ -1,5 +1,6 @@
 import { MemberListTemplate } from './util/templates.js';
 import rootApi from './api/apiHandler.js';
+import { ERROR_TYPE } from './util/constants.js';
 
 export default class KanbanMemberList {
   constructor({ teamId, $targetTodoAppListContainer }) {
@@ -10,9 +11,13 @@ export default class KanbanMemberList {
   }
 
   async render() {
-    const teamInfo = await rootApi.fetchTeam(this.teamId);
-    this.$targetTodoAppListContainer.innerHTML = MemberListTemplate(
-      teamInfo.members,
-    );
+    try {
+      const teamInfo = await rootApi.fetchTeam(this.teamId);
+      this.$targetTodoAppListContainer.innerHTML = MemberListTemplate(
+        teamInfo.members,
+      );
+    } catch (e) {
+      console.error(ERROR_TYPE.CAN_NOT_LOAD);
+    }
   }
 }
