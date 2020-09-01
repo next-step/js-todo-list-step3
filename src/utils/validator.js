1ab368dc0b2bb6b59f3903703a553919df2812b9
ykpark <read0more@gmail.com>
2020-09-01 17:14:16 +0900

    validator 리팩토링

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -2,19 +2,5 @@ export const isArray = (array) => Array.isArray(array);
 
 export const isBoolean = (value) => typeof value === "boolean";
 
-export const checkMoreThanOneClassContain = ($target, ...classNames) => {
-  const classList = $target.classList;
-  let isContain = false;
-
-  if (!classList) {
-    return isContain;
-  }
-
-  classNames.forEach((className) => {
-    if (classList.contains(className)) {
-      isContain = true;
-    }
-  });
-
-  return isContain;
-};
+export const checkMoreThanOneClassContain = ($target, ...classNames) =>
+  classNames.some((className) => $target.classList?.contains(className));
