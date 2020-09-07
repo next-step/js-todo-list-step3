f83b621ec65398e77f079923d9053fe7a30245cb
junilhwang <tjsdlf4261@naver.com>
2020-09-07 20:43:00 +0900

    feat: TeamService 작성 완료

--- src/services/TeamService.js
+++ src/services/TeamService.js
@@ -1,5 +1,23 @@
-const API_PATH = 'https://blackcoffee-todolist.df.r.appspot.com/api/temas';
+import {RestClient} from "../core/Request";
+
+const restClient = new RestClient('https://blackcoffee-todolist.df.r.appspot.com/api');
 
 export default Object.freeze({
+
+  fetchTeams () {
+    return restClient.get('/teams');
+  },
+
+  fetchTeam (teamId) {
+    return restClient.get(`/teams/${teamId}`);
+  },
+
+  addTeam (name) {
+    return restClient.post(`/teams`, { name });
+  },
+
+  deleteTeam (teamId) {
+    return restClient.delete(`/teams/${teamId}`);
+  },
 
 })
