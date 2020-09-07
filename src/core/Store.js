27dcf14dc49440e0fde229058a7be6ba31361b69
junilhwang <tjsdlf4261@naver.com>
2020-09-07 18:46:58 +0900

    feat: Core Store 추가

---
+++ src/core/Store.js
@@ -0,0 +1,43 @@
+
+export const Store = class {
+
+  $state;
+  $getters;
+  #mutations;
+  #actions;
+  #observes = new Set();
+
+  constructor({ state, mutations, getters, actions }) {
+    this.$state = state;
+    this.$getters = Object.entries(getters)
+                          .reduce((getters, [key, getter]) => (
+                            Object.defineProperty(getters, key, {
+                              get: () => getter(this.$state)
+                            }), getters), {});
+    this.#mutations = mutations;
+    this.#actions = actions;
+  }
+
+  commit (key, payload) {
+    const newState = { ...this.$state };
+    this.#mutations[key](newState, payload);
+    this.#setState(newState);
+  }
+
+  dispatch (key, payload) {
+    this.#actions[key]({
+      commit: (key, payload) => this.commit(key, payload),
+      dispatch: (key, payload) => this.dispatch(key, payload),
+      state: { ...this.$state },
+    }, payload);
+  }
+
+  addObserve (...components) {
+    components.forEach(component => this.#observes.add(component));
+  }
+
+  #setState (newState) {
+    this.$state = { ...newState };
+    this.#observes.forEach(observer => observer.$render());
+  }
+}
