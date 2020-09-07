3a59f6114ce5b81fac9f929cd4ec84fc913b3988
junilhwang <tjsdlf4261@naver.com>
2020-09-07 23:01:47 +0900

    feat: Kanban Render 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import {Team} from "./containers/Team.js";
+import {Kanban} from "./containers/Kanban.js";
 
 const $app = document.querySelector('#app');
 
-new Team($app);
+const uri = location.pathname.split('/').pop();
+
+if (uri.indexOf('index') === 0 || uri.length === 0) {
+  new Team($app);
+}
+if (uri.indexOf('kanban') === 0) {
+  new Kanban($app);
+}
