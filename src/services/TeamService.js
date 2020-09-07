f274f90cbed8ae330387dfd5e8de877ffd56facb
junilhwang <tjsdlf4261@naver.com>
2020-09-07 20:39:26 +0900

    feat: RestClient 추가

---
+++ src/services/TeamService.js
@@ -0,0 +1,5 @@
+const API_PATH = 'https://blackcoffee-todolist.df.r.appspot.com/api/temas';
+
+export default Object.freeze({
+
+})
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
-import HttpMethod from "../constants/HttpMethod";
-
 export const ONE_FRAME = 1000 / 60;
+
 export const debounceOneFrame = callback => {
   let timer = null;
   return props => {
@@ -11,9 +10,11 @@ export const debounceOneFrame = callback => {
 
 export const lazyFrame = () => new Promise(resolve => setTimeout(resolve, ONE_FRAME * 10))
 
-export const request = (url, option) => {
-  if ([HttpMethod.POST, HttpMethod.PUT, HttpMethod.PATCH].includes(option?.method)) {
-    option.headers = { ...option.headers, 'Content-Type': 'application/json' }
-  }
-  return fetch(url, option).then(response => response.json());
+export const request = url => {
+  return fetch(url).then(response => response.json());
+}
+
+export const requestWithJSONBody = (url, option) => {
+  const headers = { 'Content-Type': 'application/json' };
+  return fetch(url, { ...option, headers }).then(response => response.json());
 }
