4690da4c352f28f48191787e6e826021b1e71c07
junilHwang <tjsdlf4261@naver.com>
2020-09-17 19:32:24 +0900

    refactor: todoRouter 리팩토링 완료

---
+++ src/containers/index.ts
@@ -0,0 +1,2 @@
+export * from './Kanban';
+export * from './Team';
--- src/router/todoRouter.ts
+++ src/router/todoRouter.ts
@@ -1,14 +1,12 @@
 import { Router } from "../core/Router";
-import { Team } from "../containers/Team";
-import { Kanban } from "../containers/Kanban";
+import { Team, Kanban } from "../containers";
 
 const $app = document.querySelector('#app');
-export const todoRouter = new Router((uri: string) => {
-  console.log(uri);
-  if (uri.includes('index') || uri.length === 0) {
+export const todoRouter = new Router((uri?: string) => {
+  if (uri?.includes('index') || uri?.length === 0) {
     return new Team($app);
   }
-  if (uri.includes('kanban')) {
+  if (uri?.includes('kanban')) {
     return new Kanban($app);
   }
 });
