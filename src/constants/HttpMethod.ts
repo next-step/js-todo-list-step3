80a38e2a9ae9b3a36447dd7dca96fc9ab2ad68e1
junilHwang <tjsdlf4261@naver.com>
2020-09-17 16:15:44 +0900

    refactor: Constants 리팩토링 중

---
+++ src/constants/HttpMethod.ts
@@ -0,0 +1,7 @@
+export const enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,13 @@
+export const enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
+
+export const enum Priority {
+  NONE,
+  PRIMARY,
+  SECONDARY
+}
