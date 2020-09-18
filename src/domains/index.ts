103441577f8733e4fcf9fb4f3196763ab9e0d1c8
junilHwang <tjsdlf4261@naver.com>
2020-09-18 11:33:23 +0900

    feat: 도메인에 ComponentConstructable, RequestBody, RequestValue 추가

--- src/domains/index.ts
+++ src/domains/index.ts
@@ -1,7 +1,15 @@
+import {Component} from "../core";
+
 export interface RequestQuery { [k: string]: string }
+export type RequestBodyValue = string | boolean | number | null | undefined
+export interface RequestBody {
+  [k: string]: RequestBodyValue | RequestBodyValue[] | { [k: string]: RequestBodyValue }
+}
 export interface Constructable<T> {
   new(...args: any): T;
 }
+export type ComponentConstructable = Constructable<Component<any>>;
+
 export * from "./TodoTeam";
 export * from "./TodoMember";
 export * from "./TodoItem";
