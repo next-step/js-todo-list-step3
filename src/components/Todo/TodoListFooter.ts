642063eb222fa4fa5cc5c3eba54b6ef0e0e04222
junilHwang <tjsdlf4261@naver.com>
2020-09-19 13:33:37 +0900

    refactor: TodoListFooter Component 리팩토링 완료

--- src/components/Todo/TodoListFooter.ts
+++ src/components/Todo/TodoListFooter.ts
@@ -1,6 +1,7 @@
 import {Component} from "@/core";
 import {FilterTypes} from "@/constants";
 import {todoOfTeamStore, SET_FILTER_TYPE, DELETE_ALL_ITEM} from "@/store/todoOfTeamStore";
+import {TodoItem} from "@/domains";
 
 const filterButtons = {
   [FilterTypes.ALL]: '전체보기',
@@ -9,23 +10,24 @@ const filterButtons = {
   [FilterTypes.COMPLETED]: '완료한 일',
 };
 
-export const TodoListFooter = class extends Component {
+export const TodoListFooter = class extends Component<{ id: string }> {
 
-  get #filterType () {
+  private get filterType () {
     return todoOfTeamStore.$state.filterType[this.$props.id];
   }
 
-  get #filteredCount () {
-    return todoOfTeamStore.$getters.membersByFilteredTodoList[this.$props.id].length;
+  private get filteredCount () {
+    const memberOfItem: Record<string, any> = todoOfTeamStore.$getters.membersByFilteredTodoList;
+    return memberOfItem[this.$props.id].length;
   }
 
   template () {
     return `
-      <span class="todo-count">총 <strong>${this.#filteredCount}</strong> 개</span>
+      <span class="todo-count">총 <strong>${this.filteredCount}</strong> 개</span>
       <ul class="filters">
         ${ Object.entries(filterButtons).map(([ type, text ]) => `
           <li>
-            <a href="#" ${ this.#filterType === type ? ' class="selected"': '' } data-filter-type="${type}" data-ref="filter">${text}</a>
+            <a href="#" ${ this.filterType === type ? ' class="selected"': '' } data-filter-type="${type}" data-ref="filter">${text}</a>
           </li>
         `).join('') }
       </ul>
@@ -38,7 +40,7 @@ export const TodoListFooter = class extends Component {
       event.preventDefault();
       todoOfTeamStore.commit(SET_FILTER_TYPE, {
         memberId: this.$props.id,
-        filterType: event.target.dataset.filterType
+        filterType: (event.target as HTMLElement).dataset.filterType
       })
     });
     this.addEvent('delete-all', 'click', event => {
