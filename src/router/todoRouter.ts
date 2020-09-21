e332333c94be70891d8b558a793d2fa331b25c1f
junilHwang <tjsdlf4261@naver.com>
2020-09-21 23:34:23 +0900

    fix: router에서 index를 인식하지 못하는 문제 해결

--- src/router/todoRouter.ts
+++ src/router/todoRouter.ts
@@ -6,12 +6,10 @@ const $app = selectElement('#app');
 
 export const todoRouter = new Router((uri: string) => {
 
-  if (uri.includes('index') || uri.length === 0) {
-    return new Team($app);
-  }
-
   if (uri.includes('kanban')) {
     return new Kanban($app);
   }
+
+  return new Team($app);
 
 });
