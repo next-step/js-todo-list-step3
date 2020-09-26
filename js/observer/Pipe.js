b1e88b3da0ec31aff150ca1d9756b91bf77c3829
catsbi <killfine1988@gmail.com>
2020-09-26 20:01:26 +0900

    1. 이한솔

---
+++ js/observer/Pipe.js
@@ -0,0 +1,61 @@
+export const Pipe = class {
+    #topics;
+
+
+    constructor() {
+        this.#topics = new Map();
+    }
+
+    addTopics = (...topics) => {
+        topics.forEach(this.#addTopic);
+    }
+
+    #addTopic = (topic) => {
+        if (!this.#topics.get(topic)) {
+            this.#topics.set(topic, new Map());
+        }
+    };
+
+    deleteTopic = (topic) => {
+        this.#topics.delete(topic);
+    };
+
+    regist = ({ topic, key, handler, context }) => {
+        if (!this.#topics.get(topic)) {
+            this.#addTopic(topic);
+        }
+        const topicSet = this.#topics.get(topic);
+        topicSet.set(key, { handler, context });
+    };
+
+    unregist = ({ topic, key }) => {
+        const currentTopic = this.#topics.get(topic);
+        if (currentTopic) {
+            currentTopic.delete(key);
+        }
+    };
+
+    unregists = ({ topics, key }) => {
+        topics.forEach(topic => {
+            this.unregist({ topic, key });
+        });
+    };
+
+    notify = ({ topic, to, data }) => {
+        const targets = this.#topics.get(방번호);
+        if (targets) {
+            if (to) {
+                const toTarget = targets.get(to);
+                toTarget['handler'].call(toTarget['context'], data);
+                return ;
+            }
+            for (const [key, value] of targets) {
+                value['handler'].call(value['context'], data);
+            }
+
+        }
+
+    };
+
+
+}
