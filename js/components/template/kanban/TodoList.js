eaf8150c43d55f279cfb863448a8e9a096028b8b
eyabc <bey4314@naver.com>
2020-10-22 18:04:22 +0900

    refactor: chip 스타일 변경

--- js/components/template/kanban/TodoList.js
+++ js/components/template/kanban/TodoList.js
@@ -1,32 +1,34 @@
 import CreateElement from '../../../lib/CreateElement.js';
 
-const TodoList = () => {
-  const dom = CreateElement('section', { className: 'main ' });
 
+const TodoList = ({ todoList }) => {
+  const dom = CreateElement('section', { className: 'main ' });
+  console.log(todoList)
   // todo-list-item : completed, editing
   //  <span class="chip primary">1순위</span> secondary
   const render = () => {
     dom.innerHTML = `
     <ul class="todo-list">
+    ${todoList.map((todo) => {
+      return `
       <li class="todo-list-item">
         <div class="view">
           <input class="toggle" type="checkbox" />
           <label class="label">
-            <div class="chip-container">
               <select class="chip select">
                 <option value="0" selected>순위</option>
                 <option value="1">1순위</option>
                 <option value="2">2순위</option>
               </select>
-            </div>
             해야할 아이템
           </label>
           <button class="destroy"></button>
         </div>
         <input class="edit" value="완료된 타이틀" />
       </li>
-    </ul>
-    `
+      `
+    }).join('')}
+    </ul>`;
   };
   render();
 
