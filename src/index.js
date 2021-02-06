a01812ae84f8a215e08e99bf8ff2644dd85df533
hwookim <gusdn9754@gmail.com>
2021-02-06 20:40:07 +0900

    chore: webpack-dev-server를 통한 index.html 리다이렉팅

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import App from "./App.js";
+import "./index.css";
 
 const init = () => {
   const app = document.querySelector("#app");
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    path: path.resolve("./dist"), // 빌드 파일 생성 위치
+    filename: "[name].js", // 빌드 파일명
+  },
+  devServer: {
+    overlay: true,
+    stats: "errors-only",
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      hash: true,
+    }),
+  ],
+};
