578cdd0279cc26b2ccfda2d5125ae2e8df99c95a
Hyuneui <46099642+hongjehong@users.noreply.github.com>
2021-02-07 18:39:13 +0900

    fix : 우선 순위 클릭 시 우선순위로 정렬, 전체보기, 해야할일, 완료한일 보기 구현

--- js/TodoList.js
+++ js/TodoList.js
@@ -1,20 +1,63 @@
+import { teamID } from './Team.js';
+import { loadTodoList } from './LoadTodoList.js';
 import { todoItemTemplateHTML } from './Template.js';
+
+let filter = "all";
+
+document.addEventListener('click', event => setFilter(event));
+
+const setFilter = async event => {
+    const $as = document.querySelectorAll('a');
+    let $item;
+    let found = false;
+    $as.forEach($a => {
+        if($a.contains(event.target)){
+            $item = $a;
+            found = true;
+        }
+    })
+    if(!found) return;
+
+    const team = { _id : teamID };
+    let user = { _id : event.target.closest('li.todoapp-container').id, todoList : [] };
+    user = await loadTodoList(team, user);
+
+    filter = event.target.hash.substr(1);
+
+    renderTodoList(user);
+}
   
 export const renderTodoList = user => {
     const $todoappContainer = document.querySelector(`li.todoapp-container#${user._id}`);
     const $todoList = $todoappContainer.querySelector('ul.todo-list');
 
-    if(!user.todoList){
+    if(!user.todoList || user.todoList.length === 0){
         $todoList.innerHTML = ``;
         return;
     }
-  
-    if(user.todoList.length > 0){
-        const tempTodoList = [];
-        user.todoList.filter(item => item.priority === "FIRST").forEach(item => tempTodoList.push(item));
-        user.todoList.filter(item => item.priority === "SECOND").forEach(item => tempTodoList.push(item));
-        user.todoList.filter(item => item.priority === "NONE").forEach(item => tempTodoList.push(item));
 
-        $todoList.innerHTML = tempTodoList.map(todoItemTemplateHTML).join('');
+    if(filter === "priority"){
+        const todoListToRender = sortTodoList(user);
+        $todoList.innerHTML = todoListToRender.map(todoItemTemplateHTML).join('');
+        return;
     }
+
+    const todoListToRender = filterList(user.todoList, filter);
+    $todoList.innerHTML = todoListToRender.map(todoItemTemplateHTML).join('');
+}
+
+const sortTodoList = user => {
+    console.log(user)
+    const todoListToRender = [];
+    user.todoList.filter(item => item.priority === "FIRST").forEach(item => todoListToRender.push(item));
+    user.todoList.filter(item => item.priority === "SECOND").forEach(item => todoListToRender.push(item));
+    user.todoList.filter(item => item.priority === "NONE").forEach(item => todoListToRender.push(item));
+
+    return todoListToRender;
+}
+
+const filterList = (todoList, filter) => {
+    if(filter === "all") return todoList;
+    if(filter === "active") return todoList.filter(item => item.isCompleted === false);
+    if(filter === "completed") return todoList.filter(item => item.isCompleted === true);
 }
