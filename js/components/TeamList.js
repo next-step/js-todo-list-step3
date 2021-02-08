5cd0c0f5fe26defc828504e4e1c71fe118d239ea
skid901 <tlrudejr1993@naver.com>
2021-02-08 19:34:00 +0900

    feat(js/components/TeamList.js): Implement the render method of TeamList component

--- js/components/TeamList.js
+++ js/components/TeamList.js
@@ -1,3 +1,19 @@
+const renderTeamItem = ({ _id, name }) => `
+  <div class="team-card-container" data-id="${_id}">
+    <a href="/kanban.html" class="card">
+      <div class="card-title">${name}</div>
+    </a>
+  </div>
+`;
+
 export default function TeamApp(listEl, teamList) {
-  this.render = () => {};
+  this.render = () => {
+    listEl.innerHTML = `
+      ${teamList.teams.map(renderTeamItem).join("")}
+      <div class="add-team-button-container">
+        <button id="add-team-button" class="ripple">
+          <span class="material-icons">add</span>
+        </button>
+      </div>`;
+  };
 }
