18f088d03dc4d852ddc3445879b62a87eae1e4c4
skid901 <tlrudejr1993@naver.com>
2021-02-10 16:40:24 +0900

    feat: add MemberTitle component

--- js/components/Member/MemberApp.js
+++ js/components/Member/MemberApp.js
@@ -1,17 +1,23 @@
 import { Team } from "../../apis/index.js";
 
+import MemberTitle from "./MemberTitle.js";
+
 export default function MemberApp(appEl, teamId) {
   this.init = async () => {
-    this.titleEl = appEl.querySelector(".user-title");
-    this.listEl = appEl.querySelector(".todoapp-list-container");
+    const titleEl = appEl.querySelector("#user-title");
+    const listEl = appEl.querySelector(".todoapp-list-container");
 
     const team = await Team.getTeam(teamId);
     ({ name: this.name, members: this.members } = team);
 
+    this.memberTitle = new MemberTitle(titleEl, this);
+
     this.render();
   };
 
-  this.render = () => {};
+  this.render = () => {
+    this.memberTitle.render();
+  };
 
   this.init();
 }
