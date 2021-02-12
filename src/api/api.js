269986ae54fafa6004069c64fe32d85fbe1ff168
yujo <yujo@student.42seoul.kr>
2021-02-12 20:07:46 +0900

    feat: 기존 사용하던 api 모듈 추가

---
+++ src/api/api.js
@@ -0,0 +1,86 @@
+const BASE_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com/';
+
+const option = {
+  post: (contents) => ({
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(contents),
+  }),
+
+  delete: () => ({
+    method: 'DELETE',
+  }),
+
+  put: (contents) => ({
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(contents),
+  }),
+};
+
+const request = async (url, option = {}) => {
+  try {
+    const response = await fetch(`${BASE_URL}${url}`, option);
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return await response.json();
+  } catch (err) {
+    alert(`💣 Error : ${err} 💣`);
+  }
+};
+
+export const API = {
+  getUser: (userId) => {
+    return request(`api/users/${userId}`);
+  },
+
+  getUsers: () => {
+    return request('api/users');
+  },
+
+  addUser: (userName) => {
+    return request('api/users', option.post({ name: userName }));
+  },
+
+  deleteUser: (userId) => {
+    return request(`api/users/${userId}`, option.delete());
+  },
+
+  getUserTodos: (userId) => {
+    return request(`api/users/${userId}/items`);
+  },
+
+  addTodo: (title, userId) => {
+    return request(
+      `api/users/${userId}/items`,
+      option.post({ contents: title }),
+    );
+  },
+
+  toggleTodo: (userId, itemId) => {
+    return request(`api/users/${userId}/items/${itemId}/toggle`, option.put());
+  },
+
+  deleteTodo: (userId, itemId) => {
+    return request(`api/users/${userId}/items/${itemId}`, option.delete());
+  },
+
+  deleAllTodo: (userId) => {
+    return request(`/api/users/${userId}/items/`, option.delete());
+  },
+
+  editTodo: (newTitle, userId, itemId) => {
+    return request(
+      `/api/users/${userId}/items/${itemId}`,
+      option.put({ contents: newTitle }),
+    );
+  },
+
+  changePriority: (priority, userId, itemId) => {
+    return request(
+      `/api/users/${userId}/items/${itemId}/priority`,
+      option.put({ priority }),
+    );
+  },
+};
--- src/kanban/index.js
+++ src/kanban/index.js
@@ -1,21 +1,7 @@
-function App() {
-  const $todoApps = document.querySelector('.todoapp-list-container');
-  $todoApps.addEventListener('click', (e) => {
-    const $target = e.target;
-    const targetClassList = $target.classList;
-    if (targetClassList.contains('chip')) {
-      const $chipSelect = $target
-        .closest('.chip-container')
-        .querySelector('select');
-      $target.classList.add('hidden');
-      $chipSelect.classList.remove('hidden');
-    }
-  });
+export const Kanban = () => {
+  //
+};
 
-  const $addUserButton = document.querySelector('#add-user-button');
-  $addUserButton.addEventListener('click', () => {
-    const result = prompt('새로운 팀원 이름을 입력해주세요');
-  });
-}
-
-new App();
+window.onload = () => {
+  Kanban();
+};
