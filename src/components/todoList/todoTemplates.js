53bafd356f8bd5cffb92062c513930d468614274
Yeewon <yes3427@kookmin.ac.kr>
2021-02-15 02:03:33 +0900

    add css

--- src/components/todoList/todoTemplates.js
+++ src/components/todoList/todoTemplates.js
@@ -46,10 +46,10 @@ export const todoAppContainerTemplate = (memberId,memberName) => {
                 <div class="count-container">
                     <span class="todo-count">총 <strong>0</strong> 개</span>
                     <ul id="${memberId}"class="filters">
-                    <li> <a href="#all" id="all" class="all selected"> 전체보기 </a> </li>
-                    <li> <a href="#priority" id="priority" class="priority"> 우선 순위 </a> </li>
-                    <li> <a href="#active" id="active" class="active"> 해야할 일 </a> </li>
-                    <li> <a href="#completed" id="completed" class="completed"> 완료한 일 </a> </li>
+                    <li> <a id="all" class="all selected"> 전체보기 </a> </li>
+                    <li> <a id="priority" class="priority"> 우선 순위 </a> </li>
+                    <li> <a id="active" class="active"> 해야할 일 </a> </li>
+                    <li> <a id="completed" class="completed"> 완료한 일 </a> </li>
                     </ul>
                     <button class="clear-completed">모두 삭제</button>
                 </div>
