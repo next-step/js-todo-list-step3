55a6a37ed6e4decfb325690f5a7f4f4ddef0f6d1
domuk-k <dannyworks102@gmail.com>
2021-02-21 18:23:58 +0900

    chore: add import statement for scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /*@jsx Reilly.createElement */
 import Reilly from 'reilly';
 import ReillyDOM from 'reillyDOM';
+import './sass/index.scss';
 import App from './App';
-import './style.css';
 import configStore from './reducs/configStore';
 
 export const store = configStore();
