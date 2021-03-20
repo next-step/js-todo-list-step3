f10204616b5c3ee03cf2733cccce2bf6e0c837ef
younoah <kr_uno_10@naver.com>
2021-03-20 17:05:15 +0900

    feat : 투두아이템 우선순위 기능 구현

--- js/service/todoListService.js
+++ js/service/todoListService.js
@@ -4,6 +4,7 @@ import { api } from '../api/api.js';
 import { teamStore } from '../store/teamStore.js';
 import { memberStore } from '../store/memberStore.js';
 import { todoAppView } from '../view/todoAppView.js';
+import { PRIORITY_TYPE } from '../constant/constants.js';
 
 export default class TodoListService {
   constructor() {}
@@ -21,6 +22,19 @@ export default class TodoListService {
     return target.closest('.todo-list-item').dataset.id;
   }
 
+  activateEditMode(target) {
+    todoAppView.activateEditMode(target);
+  }
+
+  deactivateEditMode(target) {
+    todoAppView.deactivateEditMode(target);
+  }
+
+  async reLoadKanban() {
+    await this.updateMeberStore();
+    todoAppView.renderKanban(memberStore.getMembers());
+  }
+
   async updateMeberStore() {
     const teamId = teamStore.getCurrentTeam()._id;
     const team = await api.getTeam(teamId);
@@ -28,50 +42,37 @@ export default class TodoListService {
   }
 
   async deleteItem(target) {
-    console.log('TodoListService - deleteItem');
     if (!confirm('정말 삭제 하시겠습니까?')) return;
     const teamId = teamStore.getCurrentTeam()._id;
     const memberId = this.getMemberId(target);
     const itemId = this.getItemId(target);
     await api.deleteTodoItem(teamId, memberId, itemId);
-    await this.updateMeberStore();
-    todoAppView.renderKanban(memberStore.getMembers());
+    this.reLoadKanban();
   }
 
   async toggleItem(target) {
-    console.log('TodoListService - toggleItem');
     const teamId = teamStore.getCurrentTeam()._id;
     const memberId = this.getMemberId(target);
     const itemId = this.getItemId(target);
     await api.toggleTodoItem(teamId, memberId, itemId);
-    await this.updateMeberStore();
-    todoAppView.renderKanban(memberStore.getMembers());
+    this.reLoadKanban();
   }
 
   async editItem(target) {
-    console.log('TodoListService - updateItem');
-    console.log(target);
     const teamId = teamStore.getCurrentTeam()._id;
     const memberId = this.getMemberId(target);
     const itemId = this.getItemId(target);
     const text = target.value;
     await api.editTodoItem(teamId, memberId, itemId, text);
-    await this.updateMeberStore();
-    todoAppView.renderKanban(memberStore.getMembers());
+    this.reLoadKanban();
   }
 
-  activateEditMode(target) {
-    console.log('TodoListService - activateEditMode');
-    console.log(target);
-    todoAppView.activateEditMode(target);
-  }
-
-  deactivateEditMode(target) {
-    console.log('TodoListService - exitEditMode');
-    todoAppView.deactivateEditMode(target);
-  }
-
-  changePriority(target) {
-    console.log('TodoListService - changePriority');
+  async changePriority(target) {
+    const teamId = teamStore.getCurrentTeam()._id;
+    const memberId = this.getMemberId(target);
+    const itemId = this.getItemId(target);
+    const priority = PRIORITY_TYPE[target.value];
+    await api.setPriorityTodoItem(teamId, memberId, itemId, priority);
+    this.reLoadKanban();
   }
 }
