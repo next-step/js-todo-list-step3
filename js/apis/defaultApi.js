e4af84f87678cbae13bd230ed7638873197010c1
inhyuck <dlsgur1447@gmail.com>
2021-03-27 00:10:45 +0900

    refactor: api 호출 공통로직 분리

--- js/apis/defaultApi.js
+++ js/apis/defaultApi.js
@@ -1,37 +1,43 @@
 const END_POINT = 'https://js-todo-list-9ca3a.df.r.appspot.com';
 
+async function fetchWrapper({method, path, data}) {
+	let res;
+
+	if (method === 'GET' || method === 'DELETE') {
+		res = await fetch(`${END_POINT}${path}`, {
+			method,
+		});
+	}
+
+	if (method === 'POST' || method === 'PUT') {
+		res = await fetch(`${END_POINT}${path}`, {
+			method,
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+	}
+
+	//todo error process
+
+	return await res.json();
+}
+
 export default {
 	get: async function ({path}) {
-		const res = await fetch(`${END_POINT}${path}`);
-		return await res.json();
+		return await fetchWrapper({method: 'GET', path});
 	},
 
 	post: async function ({path, data}) {
-		const res = await fetch(`${END_POINT}${path}`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		});
-		return await res.json();
+		return await fetchWrapper({method: 'POST', path, data});
 	},
 
 	put: async function ({path, data}) {
-		const res = await fetch(`${END_POINT}${path}`, {
-			method: 'PUT',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		});
-		return await res.json();
+		return await fetchWrapper({method: 'PUT', path, data});
 	},
 
 	delete: async function ({path}) {
-		const res = await fetch(`${END_POINT}${path}`, {
-			method: 'DELETE',
-		});
-		return await res.json();
+		return await fetchWrapper({method: 'DELETE', path});
 	},
 };
