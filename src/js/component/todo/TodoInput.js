a0c3b7f2280fcd11fb23fd4a0907f7770848cc2a
mmtos <js223kr92@naver.com>
2021-04-25 12:07:35 +0900

    Refactor : TodoInput 이벤트 위임

--- src/js/component/todo/TodoInput.js
+++ src/js/component/todo/TodoInput.js
@@ -1,18 +1,19 @@
 import { Action } from '../../action/Action.js'
 import { $,$$ } from "../../util/domSelection.js";
 
-
-
 export class TodoInput {
-  //생성되는 시점때문에 이벤트 위임을 사용하려면 전체 ul에 걸어야 하는데 너무 광범위해 보임
   constructor() {
-    // const newTodoInput = $("input.new-todo");
-    // newTodoInput.addEventListener("keydown",async (e) => {
-    //     if (e.key == "Enter") {
-    //       //Action.addItem();
-    //       newTodoInput.value = "";
-    //     }
-    // });
+    const $app = $("ul.todoapp-list-container");
+    const teamId = $app.dataset.teamid;
+    $app.addEventListener("keydown", async ({target, key}) =>{
+      if (target.classList.contains('new-todo') && key == "Enter") {
+        const inputBox = target;
+        const $todoList = target.closest("li.todoapp-container");
+        const memberId = $todoList.dataset.memberid;
+        await Action.addItem(teamId,memberId,inputBox.value);
+        inputBox.value = "";
+      }
+    });
   }
   
   render($todoAppContainer){
@@ -22,17 +23,5 @@ export class TodoInput {
     $inputContainer.innerHTML = '';
     $inputContainer.insertAdjacentHTML('afterBegin',todoInputText);
     
-
-    const _addItemEvent = (e) =>{
-      if (e.target.nodeName=='INPUT' && e.key == "Enter") {
-        const inputBox = e.target;
-        const teamId = $todoAppContainer.closest('ul').dataset.teamid;
-        const memberId = $todoAppContainer.dataset.memberid;
-        Action.addItem(teamId,memberId,inputBox.value);
-        inputBox.value = "";
-      }
-    }
-
-    $inputContainer.addEventListener("keydown",_addItemEvent);
   }
 }
