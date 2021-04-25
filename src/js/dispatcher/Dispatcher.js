fa93e0d2a1826590b7fef9bf49a9f8063c9fb96a
mmtos <js223kr92@naver.com>
2021-04-25 11:11:09 +0900

    Fix : Store간 의존성을 반영하기 위해 동기식 반복으로 변경

--- src/js/dispatcher/Dispatcher.js
+++ src/js/dispatcher/Dispatcher.js
@@ -7,7 +7,7 @@ export class Dispatcher{
         _stores.push(store);
         return _storeCallBacks.length-1;
     };
-    dispatch(action){
+    async dispatch(action){
         var resolves = [];
         var rejects = [];
         _promises = _storeCallBacks.map((_, i) => {
@@ -16,11 +16,16 @@ export class Dispatcher{
                 rejects[i] = reject;
             });
         });
-        _storeCallBacks.forEach((storeCallBack, i) => {
-            Promise.resolve(storeCallBack.bind(_stores[i])(action))
+        for(let i=0 ; i<_storeCallBacks.length;i++){
+            Promise.resolve(await _storeCallBacks[i].bind(_stores[i])(action))
                     .then(() => resolves[i](action))
                     //.catch(() => rejects[i](new Error(`${i}번째 storeCallback 실행 실패`)));
-        });
+        }
+        // _storeCallBacks.forEach((storeCallBack, i) => {
+        //     Promise.resolve(storeCallBack.bind(_stores[i])(action))
+        //             .then(() => resolves[i](action))
+        //             //.catch(() => rejects[i](new Error(`${i}번째 storeCallback 실행 실패`)));
+        // });
         _promises = [];
     };
 }
