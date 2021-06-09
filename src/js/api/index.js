cfa9c336263fbb312346e448ff1827f8c3753d1e
dorrdorr9311 <dorrdorr9311@gmail.com>
2021-06-09 21:20:35 +0900

    api기능 작성

---
+++ src/js/api/index.js
@@ -0,0 +1,128 @@
+const headers = { 'Content-Type': "application/json"};
+
+const HTTP_METHOD = {  
+  POST(data) {
+    return {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({
+        ...data
+      }),
+    }
+  },
+  PUT(data) {
+    return {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(
+        ...data
+      ),
+    }
+  },
+  DELETE() {
+    return {
+      method: 'DELETE',
+    }
+  },
+}
+
+const api = (() => {
+  const BASE_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com/api/teams'
+  
+  const request = async (uri, config) => await fetch(BASE_URL + uri, config)
+  const requestJsonData = async (uri, config) => 
+    await fetch(BASE_URL + uri, config)
+          .then(data => data.json());
+
+  const team = {
+    add(data) {
+      return requestJsonData('', HTTP_METHOD.POST(data));
+    },
+
+    get(teamId) {
+      return requestJsonData(`/${teamId}`);
+    },
+    
+    getList() {
+      return requestJsonData('');
+    },
+
+    delete(teamId) {
+      return request(`/${teamId}`, HTTP_METHOD.DELETE());
+    }
+  },
+
+  const member = {
+    addToTeam(teamId, data) {
+      return requestJsonData(`/${teamId}/members`, HTTP_METHOD.POST(data));
+    },
+  },
+
+  const todo = {
+    getList(teamId, memberId) {
+      return requestJsonData(`/${teamId}/members/${memberId}`);
+    },
+
+    add(teamId, memberId, data) {
+      return requestJsonData(
+        `/${teamId}/members/${memberId}/items`,
+        HTTP_METHOD.POST(data)
+      );
+    },
+
+    delete(teamId,memberId, itemId) {
+      return request(
+        `/${teamId}/members/${memberId}/items/${itemId}`,
+        HTTP_METHOD.DELETE()
+      );
+    },
+
+    deleteAll(teamId,memberId) {
+      return request(
+        `/${teamId}/members/${memberId}/items`,
+        HTTP_METHOD.DELETE()
+      );
+    },
+
+    toggle(teamId, memberId, itemId, data) {
+      return requestJsonData(
+        `/${teamId}/members/${memberId}/items/${itemId}/toggle`,
+        HTTP_METHOD.PUT(data)
+      );
+    },
+
+    update(teamId, memberId, itemId, data) {
+      return requestJsonData(
+        `/${teamId}/members/${memberId}/items/${itemId}`,
+        HTTP_METHOD.PUT(data)
+      );
+    },
+
+    priority(teamId, memberId, itemId, data) {
+      return requestJsonData(
+        `/${teamId}/members/${memberId}/items/${itemId}/priority`,
+        HTTP_METHOD.PUT(data)
+      );
+    },
+  }
+
+  return {
+    team,
+    member,
+    todo,
+  }
+
+})();
+
+export default api;
+
+
+
+
+
+
+
+
+
+
+
--- src/js/team.js
+++ src/js/team.js
@@ -1,4 +1,41 @@
+import api from './api/index.js';
+
 const $addTeamButton = document.querySelector('#add-team-button')
 $addTeamButton.addEventListener('click', () => {
   const result = prompt('팀 이름을 입력해주세요')
 })
+
+const teamList = async () => {
+  const temp = await api.team.getList();
+  console.log(temp);
+  console.log(temp[0]._id);
+  const team = await api.team.get(temp[0]._id);
+  console.log(team);
+};
+
+teamList();
+
+
+const addTeam = async (name) => {
+  const newTeam = await api.team.add({name});
+  console.log(newTeam);
+}
+
+//addTeam('Dorr123');
+
+
+const deleteTeam = async (teamId) => {
+  const newTeam = await api.team.delete(teamId);
+  console.log(newTeam);
+}
+
+//deleteTeam('ctIa8wzm1');
+/*
+const teamId = teamList()[0]._id;
+
+
+const team = api.team.getTeam(teamId);
+
+
+console.log(team);
+*/
