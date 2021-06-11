700be19023756f458ffa8c0b73bde40290a9686f
iamkjw77 <iamkjw77@naver.com>
2021-06-11 18:15:38 +0900

    [Feat] API 요청 모듈 생성

---
+++ src/apis/index.js
@@ -0,0 +1,27 @@
+export const BASE_URL = 'https://js-todo-list-9ca3a.df.r.appspot.com/api/';
+export const METHOD = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+};
+
+export const fetchAPI = async (url = '', method = METHOD.GET, payload = {}) => {
+  try {
+    const option = {
+      method,
+      headers: { 'Content-Type': 'application/json' },
+    };
+
+    if (method !== METHOD.GET) {
+      option.body = JSON.stringify(payload);
+    }
+
+    const response = await fetch(`${BASE_URL}${url}`, option);
+    if (!response.ok) throw new Error(response.statusText);
+    return await response.json();
+  } catch (error) {
+    // TODO: error 객체 확인 후, error.message로 변경
+    throw new Error(error);
+  }
+};
---
+++ src/apis/todo.js
@@ -0,0 +1,42 @@
+import { fetchAPI, METHOD } from './index';
+
+// 투두 리스트 조회
+export const getTodos = (teamId, memberId) =>
+  fetchAPI(`teams/${teamId}/members/${memberId}`);
+
+// 투두 추가
+export const addTodo = (teamId, memberId, contents) =>
+  fetchAPI(`teams/${teamId}/members/${memberId}/items`, METHOD.POST, {
+    contents,
+  });
+
+// 투두 업데이트
+export const updateTodo = (teamId, memberId, itemId, contents) =>
+  fetchAPI(`teams/${teamId}/members/${memberId}/items/${itemId}`, METHOD.PUT, {
+    contents,
+  });
+
+// 투두 삭제
+export const deleteTodo = (teamId, memberId, itemId) =>
+  fetchAPI(
+    `teams/${teamId}/members/${memberId}/items/${itemId}`,
+    METHOD.DELETE
+  );
+
+// 투두 모두 삭제
+export const deleteAllTodo = (teamId, memberId) =>
+  fetchAPI(`teams/${teamId}/members/${memberId}`, METHOD.DELETE);
+
+// 투두 토글
+export const toggleTodo = (teamId, memberId, itemId) =>
+  fetchAPI(
+    `teams/${teamId}/members/${memberId}/items/${itemId}/toggle`,
+    METHOD.PUT
+  );
+
+// 우선순위 수정
+export const setPriority = (teamId, memberId, itemId) =>
+  fetchAPI(
+    `teams/${teamId}/members/${memberId}/items/${itemId}/priority`,
+    METHOD.PUT
+  );
