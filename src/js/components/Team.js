ca5b0253f04dc5f839ddc32764b144f3d7824ade
Joy <hyewon3938@naver.com>
2021-06-12 18:47:44 +0900

    refactor : MemberList 컴포넌트 대신에 Team 컴포넌트 추가
    - 설계를 변경해서 MemberList 컴포넌트를 삭제하고 Team 컴포넌트 추가

--- src/js/components/Team.js
+++ src/js/components/Team.js
@@ -1,18 +1,49 @@
+import { $ } from "../lib/util.js";
 import { fetchRequest } from "../lib/fetchRequest.js";
+import { TEMPLATE } from "../constants/template.js";
 import { API_URL, METHOD } from "../constants/config.js";
 import { INFORM_MESSAGES, ERROR_MESSAGES } from "../constants/message.js";
 
-import TodoTitle from "./TodoTitle.js";
-import MemberList from "./MemberList.js";
+import MemberModel from "./model/MemberModel.js";
+import TodoItemModel from "./model/TodoItemModel.js";
+import TeamTitle from "./TeamTitle.js";
+import Member from "./Member.js";
 
 class Team {
   constructor({ teamData }) {
     this.teamData = teamData;
-    this.memberList = new MemberList({
-      memberList: this.teamData.members,
-      onAddMember: this.onAddMember.bind(this),
+    this.memberList = this.teamData.members.map((member) => {
+      return new MemberModel({
+        ...member,
+        id: member._id,
+        todoList: member.todoList.map((item) => {
+          return new TodoItemModel({ ...item, id: item._id });
+        }),
+      });
     });
-    new TodoTitle({ titleName: this.teamData.name });
+    new TeamTitle({ titleName: this.teamData.name });
+    this.init();
+  }
+
+  init() {
+    this.render(this.memberList);
+    this.registerEventListener();
+  }
+
+  render() {
+    const template = this.memberList
+      .map((member) => {
+        return `<li class="todoapp-container" >
+        ${new Member().render(member)}
+        </li>`;
+      })
+      .join("");
+
+    $(".todoapp-list-container").innerHTML = template + TEMPLATE.ADD_MEMBER_BUTTON;
+  }
+
+  registerEventListener() {
+    $("#add-user-button").addEventListener("click", this.onAddMember.bind(this));
   }
 
   async onAddMember() {
@@ -29,7 +60,9 @@ class Team {
 
     if (error) return alert(ERROR_MESSAGES.ADD_MEMBER);
 
-    this.memberList.setState(response.members);
+    this.memberList = response.members;
+
+    this.render();
   }
 }
 
