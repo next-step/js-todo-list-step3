afca9967b026e42336d19fab6b95bcb16c3bba64
woochul2 <leewoo0686@gmail.com>
2021-06-14 01:57:51 +0900

    feat: 멤버별 todoList 불러오는 기능 구현

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,13 @@
 export const METHOD = {
   POST: 'POST',
 };
+
+export const PRIORITY = {
+  FIRST: 'FIRST',
+  NONE: 'NONE',
+  SECOND: 'SECOND',
+};
+
+export const CLASS_NAME = {
+  SELECTED: 'selected',
+};
--- src/templates.js
+++ src/templates.js
@@ -1,3 +1,5 @@
+import { CLASS_NAME, PRIORITY } from './constants.js';
+
 export const teamTemplate = ({ _id, name }) => `
   <div class="team-card-container">
     <a href="kanban.html?id=${_id}" class="card">
@@ -14,39 +16,66 @@ export const addTeamButtonTemplate = `
   </div>
 `;
 
-export const memberTemplate = ({ _id, name }) => `
-  <li class="todoapp-container">
-    <h2>
-      <span><strong>${name}</strong>'s Todo List</span>
-    </h2>
-    <div class="todoapp">
-      <section class="input-container">
-        <input class="new-todo" placeholder="할 일을 입력해주세요." autofocus />
-      </section>
-		<section class="main">
-			<ul class="todo-list"></ul>
-    </section>
-      <div class="count-container">
-        <span class="todo-count">총 <strong>0</strong> 개</span>
-        <ul class="filters">
-          <li>
-            <a href="#all" class="selected">전체보기</a>
-          </li>
-          <li>
-            <a href="#priority" class="priority">우선 순위</a>
-          </li>
-          <li>
-            <a href="#active" class="active">해야할 일</a>
-          </li>
-          <li>
-            <a href="#completed" class="completed">완료한 일</a>
-          </li>
-        </ul>
-        <button class="clear-completed">모두 삭제</button>
-      </div>
+export const todoItemTemplate = ({ _id, contents, isCompleted, priority }) => `
+  <li id="${_id}" class="todo-list-item ${isCompleted ? 'completed' : ''}">
+    <div class="view">
+      <input id="${_id}" class="toggle" type="checkbox" ${isCompleted ? 'checked' : ''}/>
+      <label class="label">
+        <div class="chip-container">
+          <select class="chip select 
+            ${priority === PRIORITY.FIRST ? 'primary' : ''}
+            ${priority === PRIORITY.SECOND ? 'secondary' : ''}
+          ">
+            <option ${priority === PRIORITY.NONE ? CLASS_NAME.SELECTED : ''}>순위</option>
+            <option ${priority === PRIORITY.FIRST ? CLASS_NAME.SELECTED : ''}>1순위</option>
+            <option ${priority === PRIORITY.SECOND ? CLASS_NAME.SELECTED : ''}>2순위</option>
+          </select>
+        </div>
+        ${contents}
+      </label>
+      <button id="${_id}" class="destroy"></button>
     </div>
+    <input class="edit" value="${contents}" />
   </li>
 `;
+
+export const memberTemplate = ({ _id, name, todoList }) => {
+  const todoListTemplate = todoList.map(todoItemTemplate);
+
+  return `
+    <li class="todoapp-container">
+      <h2>
+        <span><strong>${name}</strong>'s Todo List</span>
+      </h2>
+      <div class="todoapp">
+        <section class="input-container">
+          <input class="new-todo" placeholder="할 일을 입력해주세요." autofocus />
+        </section>
+      <section class="main">
+        <ul class="todo-list">${todoListTemplate.join('')}</ul>
+      </section>
+        <div class="count-container">
+          <span class="todo-count">총 <strong>0</strong> 개</span>
+          <ul class="filters">
+            <li>
+              <a href="#all" class="selected">전체보기</a>
+            </li>
+            <li>
+              <a href="#priority" class="priority">우선 순위</a>
+            </li>
+            <li>
+              <a href="#active" class="active">해야할 일</a>
+            </li>
+            <li>
+              <a href="#completed" class="completed">완료한 일</a>
+            </li>
+          </ul>
+          <button class="clear-completed">모두 삭제</button>
+        </div>
+      </div>
+    </li>
+  `;
+};
 
 export const addMemberButtonTemplate = `
   <li class="add-user-button-container">
