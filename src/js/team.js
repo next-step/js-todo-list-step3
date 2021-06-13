0f4fabafad9bbebf707be3db52ec9adc75f92afe
LastCow9000 <sys19912002@hanmail.net>
2021-06-14 02:49:24 +0900

    [feat] load TeamList impl

--- src/js/team.js
+++ src/js/team.js
@@ -1,4 +1,50 @@
-const $addTeamButton = document.querySelector('#add-team-button')
-$addTeamButton.addEventListener('click', () => {
-  const result = prompt('팀 이름을 입력해주세요')
-})
+import { $, $$, METHOD, API } from './util.js';
+
+const addBtn =
+  `
+    <div class="add-team-button-container">
+      <button id="add-team-button" class="ripple">
+        <span class="material-icons">add</span>
+      </button>
+    </div>
+  `;
+
+const teamTemplate = team => {
+  const teamCard =
+    `
+      <div class="team-card-container">
+      <a id=${team._id} href="/kanban.html" class="card">
+      <div class="card-title">${team.name}</div>
+      </a>
+      </div>
+    `;
+
+  return teamCard;
+};
+
+const renderTeams = teams => {
+  const $teamList = $('.team-list-container');
+  const teamList = teams.map(team => teamTemplate(team));
+
+  $teamList.innerHTML = teamList.join('') + addBtn;
+
+  const $addTeamButton = $('#add-team-button');
+  $addTeamButton.addEventListener('click', () => {
+    const result = prompt('팀 이름을 입력해주세요')
+  });
+};
+
+const loadTeams = async ({ TEAMS }) => {
+  const teams = await fetch(TEAMS)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status)
+      }
+      return res.json()
+    })
+    .catch(error => console.log(error))
+
+  renderTeams(teams);
+};
+
+loadTeams(API);
