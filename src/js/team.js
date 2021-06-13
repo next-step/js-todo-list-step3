53d081fde812f025e9d7e7852073e1d50b5c1693
LastCow9000 <sys19912002@hanmail.net>
2021-06-14 03:12:04 +0900

    [feat] add Team impl

--- src/js/team.js
+++ src/js/team.js
@@ -1,5 +1,7 @@
 import { $, $$, METHOD, API } from './util.js';
 
+const $teamList = $('.team-list-container');
+
 const addBtn =
   `
     <div class="add-team-button-container">
@@ -23,15 +25,8 @@ const teamTemplate = team => {
 };
 
 const renderTeams = teams => {
-  const $teamList = $('.team-list-container');
   const teamList = teams.map(team => teamTemplate(team));
-
   $teamList.innerHTML = teamList.join('') + addBtn;
-
-  const $addTeamButton = $('#add-team-button');
-  $addTeamButton.addEventListener('click', () => {
-    const result = prompt('팀 이름을 입력해주세요')
-  });
 };
 
 const loadTeams = async ({ TEAMS }) => {
@@ -46,5 +41,31 @@ const loadTeams = async ({ TEAMS }) => {
 
   renderTeams(teams);
 };
+
+const addTeam = (name, { TEAMS }, { POST }) => {
+  fetch(TEAMS, {
+    method: POST,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: name
+    })
+  })
+    .then(res => {
+      if (!res.ok) {
+        throw new Error(res.status)
+      }
+      loadTeams(API);
+    })
+    .catch(error => console.log(error))
+};
+
+$teamList.addEventListener('click', ({ target }) => {
+  if (target.className == 'ripple') {
+    const result = prompt('팀 이름을 입력해주세요')
+    if (result) addTeam(result, API, METHOD);
+  }
+});
 
 loadTeams(API);
