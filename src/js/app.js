bdc7018396940f9e39f622e3f2eecd8d03bb2a3d
kmkim <kmkim@triphos.co.kr>
2021-06-14 14:45:36 +0900

    refactor: 화면전환 로직 리팩토링

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,24 @@
 import Api from "../js/api/api.js";
 import TeamList from "./components/TeamList.js";
+import detail from "./detail.js";
 
 export default function App () {
-	this.teamList = new TeamList();
+	this.detail = new detail();
+	const parmas = new URLSearchParams(window.location.search);
+	const pageType = parmas.get("page");
 
+	if (!pageType) {
+		const render = async () => setState(await Api.getFetch("/api/teams"));
 
-	const setState = (teams) => {
-		this.teamList.setState(teams);
+		const setState = (teams) => this.teamList.setState(teams);
+
+		this.teamList = new TeamList(render);
+
+		// const init = () => render();
+		render();
+	}
+	else if (pageType === "teamDetail") {
+		this.detail.init();
 	}
 
-	const init = async () => {
-		const teams = await Api.getFetch("/api/teams");
-
-		console.log("teams", teams)
-
-		setState(teams);
-	}
-
-	init();
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import App from "../js/App.js";
+
+
 new App();
 
 
