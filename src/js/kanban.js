27d30bfaaf070426db941d6a645c96ea48d4885e
kmkim <kmkim@triphos.co.kr>
2021-06-14 14:06:23 +0900

    feat: todoList 우선순위, 내용 수정

--- src/js/kanban.js
+++ src/js/kanban.js
@@ -1,50 +1,67 @@
-import {$} from "./utils/querySelector.js"
+import {$, $$} from "./utils/querySelector.js"
 import Api from "../js/api/api.js";
 
 function App() {
-  let params = new URLSearchParams(window.location.search);
-  this.teamId = params.get("id");
+  this.teamId = "";
 
-  const init = () => {
-    getTeamMembers();
+  const setMemberList = async() => {
+    addMembers(await getTeamMembers());
+    addEvent();
   }
 
   const getTeamMembers = async () => {
-    let members = (await Api.getFetch(`/api/teams/${this.teamId}`)).members;
-    console.log(members);
-    setMemberList(members);
-  }
+    const params = new URLSearchParams(window.location.search);
+    this.teamId = params.get("id");
 
-  init();
+    return (await Api.getFetch(`/api/teams/${ this.teamId }`)).members;
+  };
 
-  $(".todoapp-list-container").addEventListener('click', ({target, target:{classList}}) => {
-    console.log(">>> ", window.location.search)
-    if (classList.contains('chip')) {
+  const addEvent = () => {
+    const $listContainer = $(".todoapp-list-container");
 
-      const $chipSelect = target.closest('.chip-container').querySelector('select')
+    $listContainer.addEventListener('click', ({target, target:{ value, classList }}) => {
+      console.log("classList", classList)
 
-      classList.add('hidden')
-      $chipSelect.classList.remove('hidden')
-    }
-  })
+      if (classList.contains('chip')) {
+        // const $chipSelect = target.closest('.chip-container').querySelector('select');
+        // classList.add('hidden')
+        // $chipSelect.classList.remove('hidden')
+      }
+      else if (classList.contains('destroy')) {
+      }
+      else if (classList.contains('toggle')) {
+      }
+      else if (classList.contains('chip')) {
+      }
+    });
 
-  $('#add-user-button').addEventListener('click', () => {
-    const result = prompt('새로운 팀원 이름을 입력해주세요')
-  });
+    $listContainer.addEventListener('change', ({target, target:{ value, classList }}) => {
+      if (classList.contains('chip')) changePriority(target, value);
+    });
+
+    $listContainer.addEventListener('dblclick', ({target, target:{ value, classList }}) => {
+      if (classList.contains('label')) editItem(target);
+    });
 
 
-  const setMemberList = (members) => {
+    $('#add-user-button').addEventListener('click', () => {
+      const result = prompt('새로운 팀원 이름을 입력해주세요')
+    });
+  };
+
+  const addMembers = (members) => {
     const $todoappListContainer = $(".todoapp-list-container");
+    const $addUserButtonContainer = $(".add-user-button-container").outerHTML;
     let memberItems = "";
 
     $todoappListContainer.innerHTML = "";
 
-    members.map(({ name, todoList }) => {
+    members.map(({ _id, name, todoList }) => {
       let todos = "";
       todoList.map(todoItem => todos += $new(todoItem));
 
       memberItems += `
-        <li class="todoapp-container">
+        <li class="todoapp-container" data-member-id="${_id}">
           <h2>
             <span><strong>${ name }</strong>'s Todo List</span>
           </h2>
@@ -187,14 +204,15 @@ function App() {
         </li>`;
     })
 
+    memberItems += $addUserButtonContainer;
     $todoappListContainer.innerHTML = memberItems;
-  }
+  };
 
   const $new = ({ _id, contents, isCompleted, priority }) => {
     return `
-      <li class="${ isCompleted && "completed" }" data-id="${ _id }">
+      <li class="todo-list-item ${ isCompleted && "completed" }" data-id="${ _id }">
         <div class="view">
-            <input class="toggle" type="checkbox" data-id="${ _id }" ${ isCompleted && "checked" }/>
+            <input class="toggle" type="checkbox" ${ isCompleted && "checked" }/>
             <label class="label">
                 <div class="chip-container">${ setPriority(priority) }</div>
                 ${ contents }
@@ -207,19 +225,55 @@ function App() {
 
 
   const setPriority = (priority) => {
-    let item = "";
-    if (priority === "NONE")
-      item = `<select class="chip select">
-						<option value="0" selected>순위</option>
-						<option value="1">1순위</option>
-						<option value="2">2순위</option>
-					</select>`
-    else if (priority === "FIRST") item = `<span class="chip primary">1순위</span>`
-    else if (priority === "SECOND") item = `<span class="chip secondary">2순위</span>`
+    const className = (priority === "FIRST" && "primary") || (priority === "SECOND" && "secondary");
+
+    const item = `<select class="chip select ${className}">
+                    <option value="0" ${(priority === "NONE") && "selected"}>순위</option>
+                    <option value="1" ${priority === "FIRST" && "selected"}>1순위</option>
+                    <option value="2" ${priority === "SECOND" && "selected"}>2순위</option>
+                  </select>`
 
     return item;
   }
 
+
+  const changePriority = async ($chipSelect, value) => {
+    const memberId = $chipSelect.closest(".todoapp-container").dataset.memberId;
+    const itemId = $chipSelect.closest(".todo-list-item").dataset.id;
+    let priority = "";
+
+    if (value === "1") priority = "FIRST";
+    else if (value === "2") priority = "SECOND";
+    else priority = "NONE";
+
+    await Api.putFetch(`/api/teams/${ this.teamId }/members/${ memberId }/items/${ itemId }/priority`, { priority: priority });
+    setMemberList();
+  }
+
+
+  const editItem = ($label) => {
+    const $parentLi = $label.closest(".todo-list-item");
+    const $edit = $parentLi.querySelector(".edit");
+
+    $$(".todo-list li").forEach(li => li.classList.remove("editing"));
+    $parentLi.classList.add("editing");
+
+    $edit.addEventListener("keyup",({ key, currentTarget }) => key === "Enter" && saveEditItem(currentTarget))
+  }
+
+  const saveEditItem = async ($edit) => {
+    const memberId = $edit.closest(".todoapp-container").dataset.memberId;
+    const itemId = $edit.closest(".todo-list-item").dataset.id;
+
+    await Api.putFetch(`/api/teams/${ this.teamId }/members/${ memberId }/items/${ itemId }`, {contents: $edit.value});
+    setMemberList();
+  }
+
+  const init = () => {
+    setMemberList();
+  }
+
+  init();
 
 }
 
