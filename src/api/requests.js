0c9e0652e6317d7ac16054e1718f63d81d0e2df9
Hyoungnam Kim <kimhyoungnams@gmail.com>
2021-07-22 08:37:34 +0900

    feat: :sparkles: index.html 팀 페이지 요구사항 구현

--- src/api/requests.js
+++ src/api/requests.js
@@ -3,5 +3,7 @@ import { GET, POST, DELETE, PUT } from "./http.js";
 
 export const getTeamsAPI = () => GET(`${PATH}/teams`);
 export const getTeamAPI = (teamId) => GET(`${PATH}/teams/${teamId}`);
+export const createTeamAPI = (data) => POST(`${PATH}/teams`, data)
 export const createTodoItem = (teamId, memberId, data) =>
   POST(`${PATH}/teams/${teamId}/members/${memberId}/items`, data);
+
